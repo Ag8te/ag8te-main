1edecfc3eeb2320fbed8e036626613bfa3b7bb3f
gaudencio <gaudencio@mukuru.com>
2026-03-21 21:56:16 +0200

    chore(mobile): remove unused jest.config.logic.js

--- apps/mobile/jest.config.logic.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|expo|expo-router|lucide-react-native)/)',
-  ],
-  setupFiles: ['./jest.setup.js'],
-  testEnvironment: 'node',
-};
