58748256ab702a30aa83a14c38217f8d3917d278
Thabang Ntshabeleng <ntshabelengt@gmail.com>
2026-04-01 09:41:40 +0200

    Thabang appllied fix to vite config to allow localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["mzansiserve.co.za"],
+    allowedHosts: ["localhost","mzansiserve.co.za"],
     hmr: {
       overlay: false,
     },
